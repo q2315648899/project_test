83477bf555ef0bb1e3cc9d7229abd77c3fbbf646
q2315648899 <406972433@qq.com>
2021-06-14 16:00:56 +0800

    mybatis的环境搭建

---
+++ mybatisTest/day01_eesy_01mybatis/src/main/java/com/itheima/dao/IUserDao.java
@@ -0,0 +1,20 @@
+package com.itheima.dao;
+
+import com.itheima.domain.User;
+
+import java.util.List;
+
+/**
+ * 用户的持久层接口
+ *
+ * @Author: wong
+ * @Date: 2021/6/14
+ */
+public interface IUserDao {
+    /**
+     * 查询所有用户
+     *
+     * @return
+     */
+    List<User> findAll();
+}
