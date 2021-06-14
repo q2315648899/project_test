5ad9f8ef938ec427cafd0599d6fbc6925c28146a
q2315648899 <406972433@qq.com>
2021-06-14 22:22:44 +0800

    自定义mybatis的编码-解析XML的工具类介绍

---
+++ mybatisTest/day01_eesy_04mybatis_design/src/main/java/com/itheima/mybatis/cfg/Mapper.java
@@ -0,0 +1,29 @@
+package com.itheima.mybatis.cfg;
+
+/**
+ * 用于封装执行的SQL语句和结果类型的全限定类名
+ *
+ * @Author: wong
+ * @Date: 2021/6/14
+ */
+public class Mapper {
+
+    private String queryString;//SQL
+    private String resultType;//实体类的全限定类名
+
+    public String getQueryString() {
+        return queryString;
+    }
+
+    public void setQueryString(String queryString) {
+        this.queryString = queryString;
+    }
+
+    public String getResultType() {
+        return resultType;
+    }
+
+    public void setResultType(String resultType) {
+        this.resultType = resultType;
+    }
+}
