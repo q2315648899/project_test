003ac1c5f3b8728063bf1abcd0773d34f9edd988
q2315648899 <406972433@qq.com>
2021-06-16 16:48:31 +0800

    完成account表的建立及实现单表查询

---
+++ mybatisTest/day03_eesy_03one2many/src/main/java/com/itheima/dao/IAccountDao.java
@@ -0,0 +1,19 @@
+package com.itheima.dao;
+
+import com.itheima.domain.Account;
+
+import java.util.List;
+
+/**
+ * @author 黑马程序员
+ * @Company http://www.ithiema.com
+ */
+public interface IAccountDao {
+
+    /**
+     * 查询所有账户，同时还要获取到当前账户的所属用户信息
+     * @return
+     */
+    List<Account> findAll();
+
+}
