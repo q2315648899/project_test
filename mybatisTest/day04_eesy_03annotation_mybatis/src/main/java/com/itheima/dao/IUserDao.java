e6f529ca2b96d7579d395e12ac720c98fe89fc78
q2315648899 <406972433@qq.com>
2021-06-17 16:00:47 +0800

    mybatis注解开发的环境搭建

---
+++ mybatisTest/day04_eesy_03annotation_mybatis/src/main/java/com/itheima/dao/IUserDao.java
@@ -0,0 +1,26 @@
+package com.itheima.dao;
+
+import com.itheima.domain.User;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+/**
+ * @author 黑马程序员
+ * @Company http://www.ithiema.com
+ * 在mybatis中针对,CRUD一共有四个注解
+ *  @Select @Insert @Update @Delete
+ */
+public interface IUserDao {
+
+    /**
+     * 查询所有用户
+     * @return
+     */
+    @Select("select * from user")
+    List<User> findAll();
+
+}
