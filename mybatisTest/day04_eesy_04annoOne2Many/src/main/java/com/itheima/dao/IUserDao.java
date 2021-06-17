f0b82049177b20b0eee3a2048c05e9ae40c44995
q2315648899 <406972433@qq.com>
2021-06-17 17:45:18 +0800

    mybatis注解建立实体类属性和数据库表中列的对应关系

---
+++ mybatisTest/day04_eesy_04annoOne2Many/src/main/java/com/itheima/dao/IUserDao.java
@@ -0,0 +1,51 @@
+package com.itheima.dao;
+
+import com.itheima.domain.User;
+import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.mapping.FetchType;
+
+import java.util.List;
+
+/**
+ * @author 黑马程序员
+ * @Company http://www.ithiema.com
+ * 在mybatis中针对,CRUD一共有四个注解
+ * @Select @Insert @Update @Delete
+ */
+public interface IUserDao {
+
+    /**
+     * 查询所有用户
+     *
+     * @return
+     */
+    @Select("select * from user")
+    @Results(id="userMap",value={
+            @Result(id=true,column = "id",property = "userId"),
+            @Result(column = "username",property = "userName"),
+            @Result(column = "address",property = "userAddress"),
+            @Result(column = "sex",property = "userSex"),
+            @Result(column = "birthday",property = "userBirthday"),
+    })
+    List<User> findAll();
+
+   /**
+     * 根据id查询用户
+     *
+     * @param userId
+     * @return
+     */
+    @Select("select * from user  where id=#{id} ")
+    @ResultMap("userMap")
+    User findById(Integer userId);
+
+    /**
+     * 根据用户名称模糊查询
+     *
+     * @param username
+     * @return
+     */
+    @Select("select * from user where username like #{username} ")
+    @ResultMap("userMap")
+    List<User> findUserByName(String username);
+}
