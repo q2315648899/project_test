1a367467c71b5f11aa30cfd7a2284adac7bdb520
q2315648899 <406972433@qq.com>
2021-06-18 21:37:24 +0800

    构造函数注入

--- springTest/day01_eesy_05DI/src/main/java/com/itheima/ui/client.java
+++ springTest/day01_eesy_05DI/src/main/java/com/itheima/ui/client.java
@@ -1,14 +1,8 @@
 package com.itheima.ui;
 
 import com.itheima.service.IAccountService;
-import com.itheima.service.impl.AccountServiceImpl;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 
 /**
  * 模拟一个表现层，用于调用业务层
@@ -21,33 +15,14 @@ public class client {
     /**
      * 获取spring的Ioc核心容器，并根据id获取对象
      *
-     * ApplicationContext的三个常用实现类：
-     *      ClassPathXmlApplicationContext：它可以加载类路径下的配置文件，要求配置文件必须在类路径下。不在的话，加载不了。(更常用)
-     *      FileSystemXmlApplicationContext：它可以加载磁盘任意路径下的配置文件(必须有访问权限）
-     *    上面两个基于xml配置，下面这个基于注解配置
-     *      AnnotationConfigApplicationContext：它是用于读取注解创建容器的，是明天的内容。
-     *
-     * 核心容器的两个接口引发出的问题：
-     *  ApplicationContext:     单例对象适用              采用此接口
-     *      它在构建核心容器时，创建对象采取的策略是采用立即加载的方式。也就是说，只要一读取完配置文件马上就创建配置文件中配置的对象。
-     *      （ClassPathXmlApplicationContext：xml配置了scope为单例时，ApplicationContext的实现类ClassPathXmlApplicationContext采用立即加载。配置为多例时采用延迟加载
-     *      FileSystemXmlApplicationContext：无论xml配置了scope为单例或多例，FileSystemXmlApplicationContext都采用延迟加载）
-     *
-     *  BeanFactory:            多例对象使用
-     *      它在构建核心容器时，创建对象采取的策略是采用延迟加载的方式。也就是说，什么时候根据id获取对象了，什么时候才真正的创建对象。
-     *      （BeanFactory：无论xml配置了scope为单例或多例，BeanFactory都采用延迟加载）
      * @param args
      */
     public static void main(String[] args) {
         //--------ApplicationContext----------
         //1.获取核心容器对象
-//        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
-        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
         //2.根据id获取Bean对象
-        IAccountService as  = (IAccountService)ac.getBean("accountService");
+        IAccountService as = (IAccountService) ac.getBean("accountService");
         as.saveAccount();
-
-        //手动关闭容器（如果不手动关闭容器，bean对象不会执行销毁）
-        ac.close();
     }
 }
