5ea6642a74ee250827635060b8130b6751bdefbb
q2315648899 <406972433@qq.com>
2021-06-18 17:08:11 +0800

    spring中bean的细节之三种创建Bean对象的方式

---
+++ springTest/day01_eesy_04bean/src/main/java/com/itheima/service/impl/AccountServiceImpl.java
@@ -0,0 +1,20 @@
+package com.itheima.service.impl;
+
+import com.itheima.service.IAccountService;
+
+/**
+ * 账户的业务层实现类
+ *
+ * @Author: wong
+ * @Date: 2021/6/18
+ */
+public class AccountServiceImpl implements IAccountService {
+
+    public AccountServiceImpl() {
+        System.out.println("对象创建了");
+    }
+
+    public void saveAccount() {
+        System.out.println("service中的saveAccount方法执行了。。。");
+    }
+}
---
+++ springTest/day01_eesy_04bean/src/main/java/com/itheima/ui/client.java
@@ -0,0 +1,35 @@
+package com.itheima.ui;
+
+import com.itheima.service.IAccountService;
+import com.itheima.service.impl.AccountServiceImpl;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+/**
+ * 模拟一个表现层，用于调用业务层
+ *
+ * @Author: wong
+ * @Date: 2021/6/18
+ */
+public class client {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        //--------ApplicationContext----------
+        //1.获取核心容器对象
+        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+        //2.根据id获取Bean对象
+        IAccountService as = (IAccountService) ac.getBean("accountService");
+
+        as.saveAccount();
+
+    }
+}
