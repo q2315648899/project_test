586121772a102c2750a750d54aa1b11af876b9f5
q2315648899 <406972433@qq.com>
2021-06-18 16:10:51 +0800

    ApplicationContext的三个实现类

--- springTest/day01_eesy_03spring/src/main/java/com/itheima/ui/client.java
+++ springTest/day01_eesy_03spring/src/main/java/com/itheima/ui/client.java
@@ -5,6 +5,7 @@ import com.itheima.service.IAccountService;
 import com.itheima.service.impl.AccountServiceImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 /**
  * 模拟一个表现层，用于调用业务层
@@ -17,11 +18,18 @@ public class client {
     /**
      * 获取spring的Ioc核心容器，并根据id获取对象
      *
+     * ApplicationContext的三个常用实现类：
+     *      ClassPathXmlApplicationContext：它可以加载类路径下的配置文件，要求配置文件必须在类路径下。不在的话，加载不了。(更常用)
+     *      FileSystemXmlApplicationContext：它可以加载磁盘任意路径下的配置文件(必须有访问权限）
+     *    上面两个基于xml配置，下面这个基于注解配置
+     *      AnnotationConfigApplicationContext：它是用于读取注解创建容器的，是明天的内容。
+     *
      * @param args
      */
     public static void main(String[] args) {
         //1.获取核心容器对象
         ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+//        ApplicationContext ac = new FileSystemXmlApplicationContext("C:\\Users\\wong\\Desktop\\bean.xml");
         //2.根据id获取Bean对象
         IAccountService as = (IAccountService) ac.getBean("accountService");
         IAccountDao adao = ac.getBean("accountDao", IAccountDao.class);
