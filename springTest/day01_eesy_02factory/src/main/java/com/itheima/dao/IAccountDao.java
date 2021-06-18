6b6c302731782d6a7bb8c3286daeee0cd251adc6
q2315648899 <406972433@qq.com>
2021-06-18 11:24:17 +0800

    曾经代码中的问题分析

---
+++ springTest/day01_eesy_02factory/src/main/java/com/itheima/dao/IAccountDao.java
@@ -0,0 +1,14 @@
+package com.itheima.dao;
+
+/**
+ * 账户的持久层接口
+ *
+ * @Author: wong
+ * @Date: 2021/6/18
+ */
+public interface IAccountDao {
+    /**
+     * 模拟保存账户
+     */
+    void saveAccount();
+}
---
+++ springTest/day01_eesy_02factory/src/main/java/com/itheima/ui/client.java
@@ -0,0 +1,17 @@
+package com.itheima.ui;
+
+import com.itheima.service.IAccountService;
+import com.itheima.service.impl.AccountServiceImpl;
+
+/**
+ * 模拟一个表现层，用于调用业务层
+ *
+ * @Author: wong
+ * @Date: 2021/6/18
+ */
+public class client {
+    public static void main(String[] args) {
+        IAccountService as = new AccountServiceImpl();
+        as.saveAccount();
+    }
+}
