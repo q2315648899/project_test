36c229fa77e18f8808810e411ca7e7f54e5a2de2
q2315648899 <406972433@qq.com>
2021-06-18 23:05:41 +0800

    用于创建的Component注解

--- springTest/day02_eesy_01anno_ioc/src/main/java/com/itheima/service/impl/AccountServiceImpl.java
+++ springTest/day02_eesy_01anno_ioc/src/main/java/com/itheima/service/impl/AccountServiceImpl.java
@@ -3,6 +3,7 @@ package com.itheima.service.impl;
 import com.itheima.dao.IAccountDao;
 import com.itheima.dao.impl.AccountDaoImpl;
 import com.itheima.service.IAccountService;
+import org.springframework.stereotype.Component;
 
 /**
  * 账户的业务层实现类
@@ -15,6 +16,10 @@ import com.itheima.service.IAccountService;
  *
  * 用于创建对象的
  *      他们的作用就和在XML配置文件中编写一个<bean>标签实现的功能是一样的
+ *      Component:
+ *          作用：用于把当前类对象存入spring容器中
+ *          属性：
+ *              value：用于指定bean的id。当我们不写时，它的默认值是当前类名，且首字母改小写。
  * 用于注入数据的
  *      他们的作用就和在xml配置文件中的bean标签中写一个<property>标签的作用是一样的
  * 用于改变作用范围的
@@ -24,6 +29,7 @@ import com.itheima.service.IAccountService;
  * @Author: wong
  * @Date: 2021/6/18
  */
+@Component("accountService")
 public class AccountServiceImpl implements IAccountService {
 
     private IAccountDao accountDao = new AccountDaoImpl();
