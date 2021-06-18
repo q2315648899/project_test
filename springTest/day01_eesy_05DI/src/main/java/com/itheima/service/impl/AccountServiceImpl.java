60790d22bf5b8f8a826a66349fbad75652aa7075
q2315648899 <406972433@qq.com>
2021-06-18 21:14:55 +0800

    spring的依赖注入

---
+++ springTest/day01_eesy_05DI/src/main/java/com/itheima/service/impl/AccountServiceImpl.java
@@ -0,0 +1,17 @@
+package com.itheima.service.impl;
+
+import com.itheima.service.IAccountService;
+
+/**
+ * 账户的业务层实现类
+ *
+ * @Author: wong
+ * @Date: 2021/6/18
+ */
+public class AccountServiceImpl implements IAccountService {
+
+    public void saveAccount() {
+        System.out.println("service中的saveAccount方法执行了。。。");
+    }
+
+}
