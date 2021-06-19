13e340b45c35805c97df616b95511fe42bbf320a
q2315648899 <406972433@qq.com>
2021-06-19 22:16:56 +0800

    spring的新注解-Configuration和ComponentScan

---
+++ springTest/day02_eesy_04account_annoioc_withoutxml/src/main/java/config/SpringConfiguration.java
@@ -0,0 +1,26 @@
+package config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * 该类是一个配置类，它的作用和bean.xml是一样的
+ * spring中的新注解
+ * Configuration
+ *     作用：指定当前类是一个配置类
+ *     细节：当配置类作为AnnotationConfigApplicationContext对象创建的参数时，该注解可以不写。
+ * ComponentScan
+ *      作用：用于通过注解指定spring在创建容器时要扫描的包
+ *      属性：
+ *          value：它和basePackages的作用是一样的，都是用于指定创建容器时要扫描的包。
+ *                 我们使用此注解就等同于在xml中配置了:
+ *                      <context:component-scan base-package="com.itheima"></context:component-scan>
+ */
+@Configuration
+@ComponentScan("com.itheima")
+public class SpringConfiguration {
+
+
+}
