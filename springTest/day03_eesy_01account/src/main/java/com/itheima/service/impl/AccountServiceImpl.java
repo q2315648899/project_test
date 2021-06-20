35073cc16d8aadd60acfb31e0fe09657e46d9e73
q2315648899 <406972433@qq.com>
2021-06-20 19:14:27 +0800

    案例中添加转账方法并演示事务问题

---
+++ springTest/day03_eesy_01account/src/main/java/com/itheima/service/impl/AccountServiceImpl.java
@@ -0,0 +1,63 @@
+package com.itheima.service.impl;
+
+import com.itheima.dao.IAccountDao;
+import com.itheima.domain.Account;
+import com.itheima.service.IAccountService;
+
+import java.util.List;
+
+/**
+ * 账户的业务层实现类
+ */
+public class AccountServiceImpl implements IAccountService {
+
+    private IAccountDao accountDao;
+
+    public void setAccountDao(IAccountDao accountDao) {
+        this.accountDao = accountDao;
+    }
+
+    @Override
+    public List<Account> findAllAccount() {
+        return accountDao.findAllAccount();
+    }
+
+    @Override
+    public Account findAccountById(Integer accountId) {
+        return accountDao.findAccountById(accountId);
+    }
+
+    @Override
+    public void saveAccount(Account account) {
+        accountDao.saveAccount(account);
+    }
+
+    @Override
+    public void updateAccount(Account account) {
+        accountDao.updateAccount(account);
+    }
+
+    @Override
+    public void deleteAccount(Integer acccountId) {
+        accountDao.deleteAccount(acccountId);
+    }
+
+    @Override
+    public void transfer(String sourceName, String targetName, Float money) {
+        //1.根据名称查询转出账户
+        Account source = accountDao.findAccountByName(sourceName);
+        //2.根据名称查询转入账户
+        Account target = accountDao.findAccountByName(targetName);
+        //3.转出账户减钱
+        source.setMoney(source.getMoney() - money);
+        //4.转入账户加钱
+        target.setMoney(target.getMoney() + money);
+        //5.更新转出账户
+        accountDao.updateAccount(source);
+
+        int i = 1 / 0;
+
+        //6.更新转入账户
+        accountDao.updateAccount(target);
+    }
+}
