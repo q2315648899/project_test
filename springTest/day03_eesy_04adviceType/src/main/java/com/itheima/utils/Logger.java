b60a6bb37e08d6777d4f3c8890b3575cdc63f0de
q2315648899 <406972433@qq.com>
2021-06-21 20:27:07 +0800

    四种常用通知类型

---
+++ springTest/day03_eesy_04adviceType/src/main/java/com/itheima/utils/Logger.java
@@ -0,0 +1,34 @@
+package com.itheima.utils;
+
+/**
+ * 用于记录日志的工具类，它里面提供了公共的代码
+ */
+public class Logger {
+
+    /**
+     * 前置通知
+     */
+    public  void beforePrintLog(){
+        System.out.println("前置通知Logger类中的beforePrintLog方法开始记录日志了。。。");
+    }
+
+    /**
+     * 后置通知
+     */
+    public  void afterReturningPrintLog(){
+        System.out.println("后置通知Logger类中的afterReturningPrintLog方法开始记录日志了。。。");
+    }
+    /**
+     * 异常通知
+     */
+    public  void afterThrowingPrintLog(){
+        System.out.println("异常通知Logger类中的afterThrowingPrintLog方法开始记录日志了。。。");
+    }
+
+    /**
+     * 最终通知
+     */
+    public  void afterPrintLog(){
+        System.out.println("最终通知Logger类中的afterPrintLog方法开始记录日志了。。。");
+    }
+}
