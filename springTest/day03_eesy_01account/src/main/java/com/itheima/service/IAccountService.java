e8258d9b6802d6f99622635abe21449cd1fd7e70
q2315648899 <406972433@qq.com>
2021-06-21 17:54:12 +0800

    spring基于XML的AOP-配置步骤

--- springTest/day03_eesy_01account/src/main/java/com/itheima/service/IAccountService.java
+++ springTest/day03_eesy_01account/src/main/java/com/itheima/service/IAccountService.java
@@ -52,4 +52,6 @@ public interface IAccountService {
      * @param money      转账金额
      */
     void transfer(String sourceName, String targetName, Float money);
+
+    //void test();//它只是连接点，但不是切入点，因为没有被增强
 }
