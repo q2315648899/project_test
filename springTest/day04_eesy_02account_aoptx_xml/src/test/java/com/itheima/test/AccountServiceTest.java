191cef778e72f1b322dc3409f7e27082e5cd5a45
q2315648899 <406972433@qq.com>
2021-06-22 14:55:51 +0800

    基于XML的AOP实现事务控制

---
+++ springTest/day04_eesy_02account_aoptx_xml/src/test/java/com/itheima/test/AccountServiceTest.java
@@ -0,0 +1,30 @@
+package com.itheima.test;
+
+import com.itheima.domain.Account;
+import com.itheima.service.IAccountService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+/**
+ * 使用Junit单元测试：测试我们的配置
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:bean.xml")
+public class AccountServiceTest {
+
+    @Autowired
+    private IAccountService as;
+
+    @Test
+    public void testTransfer() {
+        as.transfer("aaa", "bbb", 100f);
+    }
+}
