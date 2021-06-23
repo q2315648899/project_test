0a2b422cc4f572727c007f3261389cb8712fe6bd
q2315648899 <406972433@qq.com>
2021-06-23 16:06:34 +0800

    ModelAttribute注解

--- springMVCTest/springmvc_day01_01_start/src/main/java/cn/itcast/controller/AnnoController.java
+++ springMVCTest/springmvc_day01_01_start/src/main/java/cn/itcast/controller/AnnoController.java
@@ -75,6 +75,44 @@ public class AnnoController {
         System.out.println(cookieValue);
         return "success";
     }
+
+    /**
+     * ModelAttribute注解
+     * @return
+     */
+    @RequestMapping(value="/testModelAttribute")
+    public String testModelAttribute(@ModelAttribute("abc") User user){
+        System.out.println("testModelAttribute执行了...");
+        System.out.println(user);
+        return "success";
+    }
+
+    @ModelAttribute
+    public void showUser(String uname, Map<String,User> map){
+        System.out.println("showUser执行了...");
+        // 通过用户查询数据库（模拟）
+        User user = new User();
+        user.setUname(uname);
+        user.setAge(20);
+        user.setDate(new Date());
+        map.put("abc",user);
+    }
+
+    /**
+     * 该方法会先执行
+
+     @ModelAttribute
+     public User showUser(String uname){
+     System.out.println("showUser执行了...");
+     // 通过用户查询数据库（模拟）
+     User user = new User();
+     user.setUname(uname);
+     user.setAge(20);
+     user.setDate(new Date());
+     return user;
+     }
+     */
+
 }
 
 
--- springMVCTest/springmvc_day01_01_start/src/main/webapp/anno.jsp
+++ springMVCTest/springmvc_day01_01_start/src/main/webapp/anno.jsp
@@ -35,5 +35,12 @@
     <!-- CookieValue 注解 -->
     <a href="anno/testCookieValue">CookieValue</a>
 
+    <br>
+
+    <form action="anno/testModelAttribute" method="post">
+        用户姓名：<input type="text" name="uname" /><br/>
+        用户年龄：<input type="text" name="age" /><br/>
+        <input type="submit" value="提交" />
+    </form>
 </body>
 </html>
