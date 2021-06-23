263fa76d5b7025b6eee54ecab817241f7fc10e32
q2315648899 <406972433@qq.com>
2021-06-23 14:44:56 +0800

    RequestParam、RequestBody注解

---
+++ springMVCTest/springmvc_day01_01_start/src/main/java/cn/itcast/controller/AnnoController.java
@@ -0,0 +1,55 @@
+package cn.itcast.controller;
+
+import cn.itcast.domain.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * 常用的注解
+ */
+@Controller
+@RequestMapping("/anno")
+@SessionAttributes(value={"msg"})   // 把msg=美美存入到session域对中
+public class AnnoController {
+
+    @RequestMapping("/testRequestParam")
+    public String testRequestParam(@RequestParam(name="name") String username){
+        System.out.println("执行了...");
+        System.out.println(username);
+        return "success";
+    }
+
+    /**
+     * 获取到请求体的内容
+     * @return
+     */
+    @RequestMapping("/testRequestBody")
+    public String testRequestBody(@RequestBody String body){
+        System.out.println("执行了...");
+        System.out.println(body);
+        return "success";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ springMVCTest/springmvc_day01_01_start/src/main/webapp/anno.jsp
@@ -0,0 +1,28 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2018/4/29
+  Time: 23:35
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+    <%--常用的注解--%>
+
+    <a href="anno/testRequestParam?name=哈哈">RequestParam</a>
+
+    <br>
+
+    <form action="anno/testRequestBody" method="post">
+        用户姓名：<input type="text" name="username" /><br/>
+        用户年龄：<input type="text" name="age" /><br/>
+        <input type="submit" value="提交" />
+    </form>
+
+</body>
+</html>
