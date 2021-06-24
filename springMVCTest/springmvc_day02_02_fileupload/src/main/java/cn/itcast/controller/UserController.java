8fada4cca9bdf719c0ee7a6de7fa2252435f6a74
q2315648899 <406972433@qq.com>
2021-06-24 15:31:04 +0800

    文件上传之跨服务器上传代码

--- springMVCTest/springmvc_day02_02_fileupload/src/main/java/cn/itcast/controller/UserController.java
+++ springMVCTest/springmvc_day02_02_fileupload/src/main/java/cn/itcast/controller/UserController.java
@@ -1,5 +1,7 @@
 package cn.itcast.controller;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
@@ -15,6 +17,36 @@ import java.util.UUID;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    /**
+     * 跨服务器文件上传
+     * @return
+     */
+    @RequestMapping("/fileupload3")
+    public String fileuoload3(MultipartFile upload) throws Exception {
+        System.out.println("跨服务器文件上传...");
+
+        // 定义上传文件服务器路径
+        String path = "http://localhost:9090/uploads/";
+
+        // 说明上传文件项
+        // 获取上传文件的名称
+        String filename = upload.getOriginalFilename();
+        // 把文件的名称设置唯一值，uuid
+        String uuid = UUID.randomUUID().toString().replace("-", "");
+        filename = uuid+"_"+filename;
+
+        // 创建客户端的对象
+        Client client = Client.create();
+
+        // 和图片服务器进行连接
+        WebResource webResource = client.resource(path + filename);
+
+        // 上传文件
+        webResource.put(upload.getBytes());
+
+        return "success";
+    }
 
     /**
      * SpringMVC文件上传
--- springMVCTest/springmvc_day02_02_fileupload/src/main/webapp/index.jsp
+++ springMVCTest/springmvc_day02_02_fileupload/src/main/webapp/index.jsp
@@ -27,5 +27,12 @@
         <input type="submit" value="上传" />
     </form>
 
+    <h3>跨服务器文件上传</h3>
+
+    <form action="/user/fileupload3" method="post" enctype="multipart/form-data">
+        选择文件：<input type="file" name="upload" /><br/>
+        <input type="submit" value="上传" />
+    </form>
+
 </body>
 </html>
