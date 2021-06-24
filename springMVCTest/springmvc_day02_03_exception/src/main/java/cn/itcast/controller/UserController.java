aba6512a9bd1e79b9e88bc5fbac96162618e9b61
q2315648899 <406972433@qq.com>
2021-06-24 17:13:09 +0800

    SpringMVC异常处理之异常处理代码编写

--- springMVCTest/springmvc_day02_03_exception/src/main/java/cn/itcast/controller/UserController.java
+++ springMVCTest/springmvc_day02_03_exception/src/main/java/cn/itcast/controller/UserController.java
@@ -1,5 +1,6 @@
 package cn.itcast.controller;
 
+import cn.itcast.exception.SysException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,10 +10,19 @@ public class UserController {
 
 
     @RequestMapping("/testException")
-    public String testException() throws Exception {
+    public String testException() throws SysException {
         System.out.println("testException执行了...");
-        // 模拟异常
-        int a = 10 / 0;
+
+        try {
+            // 模拟异常
+            int a = 10/0;
+        } catch (Exception e) {
+            // 打印异常信息
+            e.printStackTrace();
+            // 抛出自定义异常信息
+            throw new SysException("查询所有用户出现错误了...");
+        }
+
         return "success";
     }
 
