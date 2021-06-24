ff602a4fd84ffb25eace6e4a7ad4d8a4d5caefcf
q2315648899 <406972433@qq.com>
2021-06-24 14:32:51 +0800

    文件上传之Springmvc方式上传代码

--- springMVCTest/springmvc_day02_02_fileupload/src/main/java/cn/itcast/controller/UserController.java
+++ springMVCTest/springmvc_day02_02_fileupload/src/main/java/cn/itcast/controller/UserController.java
@@ -5,6 +5,7 @@ import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
@@ -14,6 +15,36 @@ import java.util.UUID;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    /**
+     * SpringMVC文件上传
+     * @return
+     */
+    @RequestMapping("/fileupload2")
+    public String fileuoload2(HttpServletRequest request, MultipartFile upload) throws Exception {
+        System.out.println("springmvc文件上传...");
+
+        // 使用fileupload组件完成文件上传
+        // 上传的位置
+        String path = request.getSession().getServletContext().getRealPath("/uploads/");
+        // 判断，该路径是否存在
+        File file = new File(path);
+        if(!file.exists()){
+            // 创建该文件夹
+            file.mkdirs();
+        }
+
+        // 说明上传文件项
+        // 获取上传文件的名称
+        String filename = upload.getOriginalFilename();
+        // 把文件的名称设置唯一值，uuid
+        String uuid = UUID.randomUUID().toString().replace("-", "");
+        filename = uuid+"_"+filename;
+        // 完成文件上传
+        upload.transferTo(new File(path,filename));
+
+        return "success";
+    }
 
     /**
      * 文件上传
@@ -60,5 +91,4 @@ public class UserController {
 
         return "success";
     }
-
 }
--- springMVCTest/springmvc_day02_02_fileupload/src/main/webapp/index.jsp
+++ springMVCTest/springmvc_day02_02_fileupload/src/main/webapp/index.jsp
@@ -8,15 +8,24 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>文件上传</title>
+    <title>Title</title>
+
+</head>
+<body>
+
+    <h3>传统文件上传</h3>
 
     <form action="/user/fileupload1" method="post" enctype="multipart/form-data">
         选择文件：<input type="file" name="upload"/><br/>
         <input type="submit" value="上传"/>
     </form>
 
-</head>
-<body>
+    <h3>Springmvc文件上传</h3>
+
+    <form action="/user/fileupload2" method="post" enctype="multipart/form-data">
+        选择文件：<input type="file" name="upload" /><br/>
+        <input type="submit" value="上传" />
+    </form>
 
 </body>
 </html>
