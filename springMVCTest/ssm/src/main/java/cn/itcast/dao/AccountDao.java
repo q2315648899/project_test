536f6b4df7503ce6b59410b63f33c37d48c8218d
q2315648899 <406972433@qq.com>
2021-06-25 17:52:48 +0800

    ssm整合之编写MyBatis框架

--- springMVCTest/ssm/src/main/java/cn/itcast/dao/AccountDao.java
+++ springMVCTest/ssm/src/main/java/cn/itcast/dao/AccountDao.java
@@ -13,9 +13,11 @@ import java.util.List;
 public interface AccountDao {
 
     // 查询所有账户
+    @Select("select * from account")
     public List<Account> findAll();
 
     // 保存帐户信息
+    @Insert("insert into account (name,money) values (#{name},#{money})")
     public void saveAccount(Account account);
 
 }
