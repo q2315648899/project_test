a977ac95d318338b2d8d931e9a1d587a4bbbf846
q2315648899 <406972433@qq.com>
2021-06-29 00:31:05 +0800

    ssm整合之Spring整合MyBatis框架配置事务

--- springMVCTest/ssm/src/main/java/cn/itcast/dao/AccountDao.java
+++ springMVCTest/ssm/src/main/java/cn/itcast/dao/AccountDao.java
@@ -10,6 +10,7 @@ import java.util.List;
 /**
  * 帐户dao接口
  */
+//@Repository//此注解可有可无，因为当前标注的时接口无法生成bean，在applicationContext.xml中通过MapperScannerConfigurer在编译时生成动态代理对象
 public interface AccountDao {
 
     // 查询所有账户
