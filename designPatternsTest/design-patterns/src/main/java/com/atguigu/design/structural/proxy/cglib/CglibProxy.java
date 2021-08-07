f1cefec1f976de0d4d5eab3c56a2f7d05032acd6
q2315648899 <406972433@qq.com>
2021-08-07 18:34:09 +0800

    23种设计模式和框架源码

---
+++ designPatternsTest/design-patterns/src/main/java/com/atguigu/design/structural/proxy/cglib/CglibProxy.java
@@ -0,0 +1,45 @@
+package com.atguigu.design.structural.proxy.cglib;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * 1、使用cglib帮我们创建出代理对象
+ */
+public class CglibProxy {
+
+    //为任意对象创建代理
+    public static<T> T createProxy(T t){
+        //1、创建一个增强器
+        Enhancer enhancer = new Enhancer();
+
+        //2、设置要增强哪个个类的功能。增强器为这个类动态创建一个子类
+        enhancer.setSuperclass(t.getClass());
+
+        //3、设置回调
+        enhancer.setCallback(new MethodInterceptor() {
+            @Override
+            public Object intercept(Object obj,
+                                    Method method,  //为了能获取到原方法的一些元数据信息
+                                    Object[] args,
+                                    MethodProxy proxy) throws Throwable {
+                //编写拦截的逻辑
+                System.out.println("cglib上场le .......xxx");
+                //当前方法的信息
+//                method.get
+//                method.getAnnotation()
+                //目标方法进行执行
+                Object invoke = proxy.invokeSuper(obj,args);//obj是代理对象引用，proxy.invokeSuper(obj,args)表示反射执行代理对象的父类（也就是被代理对象）的方法
+//                Object invoke = proxy.invoke(t,args);//t是被代理对象，proxy.invoke(t,args)表示反射执行被代理对象的方法（和上面的效果一样），此处也可用method.invoke(t,args)
+                return invoke;
+            }
+        });
+
+        Object o = enhancer.create();
+        return (T) o;
+    }
+
+}
